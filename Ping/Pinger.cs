b10999070f1ce35a2868ea60b006e61d3112f15d
Vivian <test@test.de>
2026-04-23 08:56:16 +0200

    Projektdateien hinzufügen.

---
+++ Ping/Pinger.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Ping
+{
+    internal class Pinger
+    {
+        private int Delay { get; set; }
+        private int Amount { get; set; }
+        private bool SendInfinite { get; set; }
+        private bool ResolveIP { get; set; }
+        public IPAddress HostAddress { get; set; }
+        public IPHostEntry? Host { get; set; }
+
+        public Pinger(string Host, int? delay = 0, int? amount = 5, bool sendInfinite = false, bool resolveIP = false)
+        {
+            Delay = delay ?? 0;
+            Amount = amount ?? 5;
+            SendInfinite = sendInfinite;
+            ResolveIP = resolveIP;
+
+            if (!IPAddress.IsValid(Host))
+            {
+                this.Host = Dns.GetHostEntry(Host);
+                HostAddress = this.Host.AddressList[RandomNumberGenerator.GetInt32(this.Host.AddressList.Length)];
+            }
+            else
+            {
+                HostAddress = IPAddress.Parse(Host);
+                this.Host = null;
+            }
+
+            if (ResolveIP && this.Host is null)
+            {
+                this.Host = Dns.GetHostEntry(Host);
+            }
+        }
+
+        public async Task ResolveIPAddress()
+        {
+            Host = await Dns.GetHostEntryAsync(HostAddress);
+        }
+
+        public async Task<List<PingReply>> SendPing(bool Verbosity = false)
+        {
+            List<PingReply> results = [];
+            using System.Net.NetworkInformation.Ping p = new();
+
+            if (!SendInfinite)
+            {
+                for (int i = 0; i < Amount; i++)
+                {
+                    results.Add(await Send(p, Verbosity));
+                }
+            }
+            else
+            {
+                while (SendInfinite)
+                {
+                    await Send(p, Verbosity);
+                }
+            }
+
+            return results;
+        }
+
+        private async Task<PingReply> Send(System.Net.NetworkInformation.Ping p, bool Verbosity = false)
+        {
+            PingReply r = await p.SendPingAsync(HostAddress);
+
+            if (Verbosity)
+            {
+                EvaluateReply(r);
+            }
+
+            await Task.Delay(Delay);
+
+            return r;
+        }
+
+        private static void EvaluateReply(PingReply r)
+        {
+            switch (r.Status)
+            {
+                case IPStatus.Success:
+                    Console.WriteLine($"Reply from {r.Address}: Bytes={r.Buffer.Length} RTT={r.RoundtripTime} TTL={r.Options?.Ttl}");
+                    break;
+                case IPStatus.BadRoute:
+                    Console.WriteLine("Invalide Route");
+                    break;
+                case IPStatus.DestinationHostUnreachable:
+                    Console.WriteLine("Destination Host Unreachable");
+                    break;
+                case IPStatus.DestinationNetworkUnreachable:
+                    Console.WriteLine("DestinationNetworkUnreachable");
+                    break;
+                case IPStatus.DestinationPortUnreachable:
+                    Console.WriteLine("Destination Port Unreachable");
+                    break;
+                case IPStatus.BadHeader:
+                    Console.WriteLine("Bad Header");
+                    break;
+                case IPStatus.BadDestination:
+                    Console.WriteLine("Bad Destination");
+                    break;
+                case IPStatus.BadOption:
+                    Console.WriteLine("Invalid ICMP option");
+                    break;
+                case IPStatus.TimedOut:
+                    Console.WriteLine("Request timed out");
+                    break;
+                case IPStatus.TtlExpired:
+                    Console.WriteLine("TTL Expired");
+                    break;
+            }
+        }
+    }
+}
---
+++ Ping/Program.cs
@@ -0,0 +1,78 @@
+﻿using Ping;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+
+StringBuilder sb = new();
+
+if (args.Length == 0)
+{
+    sb.Append(Console.ReadLine());
+}
+else
+{
+    sb.Append(args[0]);
+}
+
+string HostString = sb.ToString();
+
+Pinger p = new(HostString, GetDelay(args), GetAmount(args), GetSI(args), GetResolveIP(args));
+
+if (p.Host is null)
+{
+    Console.WriteLine($"Pinging {p.HostAddress}:");
+}
+else
+{
+    Console.WriteLine($"Pinging {p.HostAddress}[{p.Host?.HostName}]");
+}
+
+List<PingReply> replies = await p.SendPing(true);
+
+Console.WriteLine($"\nSend = {replies.Count}\tReceived = {replies.Where(x => x.Status == IPStatus.Success).Count()}\tLost = {replies.Where(x => x.Status != IPStatus.Success).Count()}");
+Console.WriteLine($"Max RTT = {replies.Max(x => x.RoundtripTime)}\tMin RTT = {replies.Min(x => x.RoundtripTime)}\tAverage RTT = {GetAverageRTT(replies)}");
+
+static long GetAverageRTT(List<PingReply> replies)
+{
+    long avg = 0;
+
+    for (int i = 0; i < replies.Count; i++)
+    {
+        avg += replies[i].RoundtripTime;
+    }
+
+    return avg / replies.Count;
+}
+
+
+static bool GetSI(string[] args)
+{
+    return args.Contains("t");
+}
+
+static bool GetResolveIP(string[] args)
+{
+    return args.Contains("a");
+}
+
+static int? GetDelay(string[] args)
+{
+    if (args.Contains("d"))
+    {
+        return int.Parse(args[args.IndexOf("d") + 1]);
+    }
+    return null;
+}
+
+static int? GetAmount(string[] args)
+{
+    if (args.Contains("n"))
+    {
+        return int.Parse(args[args.IndexOf("n") + 1]);
+    }
+    return null;
+}
